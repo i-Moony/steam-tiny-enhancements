cf7bd1c52a9ee4d4a79a65ea6506f2083dbe123f
m00ny <danila.kononov.official@gmail.com>
2023-12-04 00:59:12 +0300

    chore: setup vite-plugin-web-extension

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from "vite";
+import vuePlugin from "@vitejs/plugin-vue";
+import webExtension from "vite-plugin-web-extension";
+
+export default defineConfig
+({
+    plugins:
+    [
+        webExtension
+        ({
+            browser: "firefox",
+            webExtConfig:
+            {
+                firefox: "firefoxdeveloperedition",
+            },
+        }),
+        vuePlugin(),
+    ],
+});
