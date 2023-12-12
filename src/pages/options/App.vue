37b2f21325c275bdcb573d56f2e0b39a387b5e1d
m00ny <danila.kononov.official@gmail.com>
2023-12-13 00:21:21 +0300

    chore: remove unused backgrounds

--- src/pages/options/App.vue
+++ src/pages/options/App.vue
@@ -10,9 +10,6 @@
 <style>
     html
     {
-        /* background: #2a2d34; */
-        /* background: radial-gradient(ellipse at top, #3e424d, #2a2d34); */
-        /* background: #1d2229; */
         background: #2d3036;
         min-width: max(100%, 1000px);
         min-height: max(100%, 600px);
